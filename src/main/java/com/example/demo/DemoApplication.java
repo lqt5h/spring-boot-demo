21b6b8d63ef432a4c7fdd980493f20d236820f23
Mareychenko Fedor <pfoasn@gmail.com>
2025-12-02 21:02:50 +0300

    Add TLS certificates and CI workflow

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,9 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String hash = encoder.encode("Admin@12345");
+        System.out.println("BCrypt hash for Admin@12345: " + hash);
     }
 
 }
